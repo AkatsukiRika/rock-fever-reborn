afd28cd465a03b8ae9e5b29cf406c16384690e5d
AkatsukiRika <m1345860061@gmail.com>
2021-07-10 15:30:27 +0800

    删除并更换乐曲

--- Assets/Scripts/PlaySettings.cs
+++ Assets/Scripts/PlaySettings.cs
@@ -12,7 +12,7 @@ public static class PlaySettings {
      * 若音频比谱面快，将此值设定为负数，音频将延后播放，谱面中的判定时间将提前；
      * 若谱面比音频快，将此值设定为正数，谱面将延后播放，谱面中的判定时间将延后；
      */
-    public static float latency = LATENCY_MACOS;
+    public static float latency = LATENCY_WIN;
 
     /**
      * 乐曲的BPM（每分钟节拍数）
@@ -27,5 +27,5 @@ public static class PlaySettings {
     /**
      * 谱面流动速度，默认的1.0速为1个四分音符下落Unity中的1个单位
      */
-    public static float hiSpeed = 3.0f;
+    public static float hiSpeed = 5.0f;
 }
